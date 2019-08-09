b1edd9ac40635b1413d87f926c8a549453d158a6
oleariyah <oleariyah@gmail.com>
2019-08-09 14:48:44 +0100

    style updated

--- src/components/LoaderCard.js
+++ src/components/LoaderCard.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 export default () => {
   return (
     <DivWrapper>
-      <div classNAme="container">
+      <div className="container">
         <div className="row">
           <div className="col-lg-4 col-xs-12 col-sm-12 mt-2">
             <div className="card p-3">
