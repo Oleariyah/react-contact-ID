25b53613da35eb0e0caebc733c241ae46a53921f
oleariyah <oleariyah@gmail.com>
2019-08-09 12:19:52 +0100

    change cursor to pointer

--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -52,6 +52,7 @@ export default ({ detail, handleClick }) => {
 };
 const DivWrapper = styled.div`
   .card {
+      cursor: pointer;
       width: 400px;
       box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
   }
